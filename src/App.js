477fa44066cb7cf9f996f827589df141e96e12ac
AleksMaifet <aleksmaifet@gmail.com>
2022-10-31 20:11:10 +0300

    innit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {
+	DEFAULT_TOAST_ANIMATION,
+	DEFAULT_TOAST_POSITION,
+	DEFAULT_TOAST_TYPE,
+	handleToastService,
+	ToastContainer
+} from "@aleksmaifet/toast_library";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const props = {
+		animation: DEFAULT_TOAST_ANIMATION.LEFT,
+		position: DEFAULT_TOAST_POSITION.BOTTOM_LEFT,
+		content: 'Hello',
+		label: 'Label',
+		backgroundColor: 'pink',
+		color: 'yellow',
+	}
+
+	const props2 = {
+		type: DEFAULT_TOAST_TYPE.WARNING,
+		animation: DEFAULT_TOAST_ANIMATION.RIGHT,
+		position: DEFAULT_TOAST_POSITION.TOP_RIGHT,
+		autoClose: true,
+		spacing: {
+			top: 30,
+			right: 50,
+		}
+	}
+
+	const onSetOptions = (value) => () => {
+		handleToastService(value)
+	}
+
+	return (
+		<div className="App">
+			<ToastContainer/>
+			<button onClick={onSetOptions(props)}>First Toast</button>
+			<br/>
+			<button onClick={onSetOptions(props2)}>Second Toast</button>
+		</div>
+	);
 }
 
 export default App;
+
+
